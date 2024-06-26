168752294f638277d6e14d175aa9947fea861eb6
sudarshan raveendranath dayarathna <120754357+sudarshan-raveendranath@users.noreply.github.com>
2024-06-26 22:43:23 +0530

    new file:   Car_rental_system/src/main/java/org/wsrd/car_rental_system/configuration/SimpleCorsFilter.java 	modified:   car_rental_angular/src/app/auth/components/signup/signup.component.ts 	modified:   car_rental_angular/src/app/auth/services/auth/auth.service.ts

--- car_rental_angular/src/app/auth/components/signup/signup.component.ts
+++ car_rental_angular/src/app/auth/components/signup/signup.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../../services/auth/auth.service';
+import { NzMessageService } from 'ng-zorro-antd/message';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-signup',
@@ -11,7 +14,10 @@ export class SignupComponent {
   isSpinning: boolean = false;
   signupForm!: FormGroup;
 
-  constructor(private fb: FormBuilder) {}
+  constructor(private fb: FormBuilder, 
+    private authService: AuthService, 
+    private message:NzMessageService,
+    private router:Router ) {}
 
   ngOnInit() {
     this.signupForm = this.fb.group({
@@ -33,6 +39,15 @@ export class SignupComponent {
 
   register() {
     console.log(this.signupForm.value);
+    this.authService.register(this.signupForm.value).subscribe((res) => {
+      console.log(res);
+      if (res.id != null) {
+        this.message.success("User registered successfully",{nzDuration: 3000});
+        this.router.navigateByUrl("/login");
+      }else {
+        this.message.error("User registration failed",{nzDuration: 3000});
+      }
+    });
   }
   
 
--- car_rental_angular/src/app/auth/services/auth/auth.service.ts
+++ car_rental_angular/src/app/auth/services/auth/auth.service.ts
@@ -1,9 +1,17 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+const BASE_URL = ["http://localhost:8080"];
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(private htpp: HttpClient) { }
+
+  register(signupRequest: any): Observable<any> {
+    return this.htpp.post(BASE_URL + "/api/auth/signup", signupRequest);
+  }
 }
