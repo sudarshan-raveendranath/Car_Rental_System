2187ba4aaf704a0e1c01f060ec7b82ac46cba29f
sudarshan raveendranath dayarathna <120754357+sudarshan-raveendranath@users.noreply.github.com>
2024-06-30 15:02:30 +0530

    new file:   .vscode/settings.json 	modified:   car_rental_angular/src/app/app.component.ts

--- car_rental_angular/src/app/app.component.ts
+++ car_rental_angular/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { StorageService } from './auth/services/storage/storage.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'car_rental_angular';
+
+  isCustomerLoggedIn: boolean = StorageService.isCustomerLoggedIn();
+  isAdminLoggedIn: boolean = StorageService.isAdminLoggedIn();
+
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    this.router.events.subscribe(() => {
+      if(event.constructor.name === "NavigationEnd") {
+        this.isCustomerLoggedIn = StorageService.isCustomerLoggedIn();
+        this.isAdminLoggedIn = StorageService.isAdminLoggedIn();
+      }
+  }
 }
